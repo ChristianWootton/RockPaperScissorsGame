d6e067debc44e1bfa3d7405134c129f0a57865a2
Christian Wootton <cwootton162@gmail.com>
2021-03-10 22:37:33 +0000

    Base Class
    Took an approach to create a method to distinguish outcome parameters. Seems to take issue with representing the user input, always results in win case.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RockPaperScissorsGame
  Small Tutorial/Project
+Small tutorial by https://www.youtube.com/watch?v=WJCNxT2bolg&ab_channel=MeNamesMarty
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -3,7 +3,48 @@
 namespace RockPaperScissors{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Hello World!");
-        }
+            string inputPlayer, inputEnemy, result;
+            int randomInt;
+
+            Console.WriteLine("Choose 1:Rock, 2:Paper or 3:Scissors.");
+            inputPlayer = Console.ReadLine();
+
+            Random numberGnerated = new Random();
+            randomInt = numberGnerated.Next(1,4);
+            //Testing purposes. 
+            //randomInt = 2;
+            //inputPlayer = "Paper";
+            
+            
+            Console.WriteLine(randomInt);
+
+            switch (randomInt)
+            {
+                case 1: 
+                    inputEnemy = "Rock";
+                    endGameScenario();
+                    break;
+                case 2: 
+                    inputEnemy = "Paper";
+                    endGameScenario();
+                    break;
+                case 3: 
+                    inputEnemy = "Scissors";
+                    endGameScenario();
+                    break;
+            }
+            void endGameScenario(){
+                if (inputEnemy == inputPlayer){
+                    result = "DRAW!";
+                }
+                else if ((inputPlayer == "Rock" && randomInt == 2) || (inputPlayer == "Paper" && randomInt == 3) || (inputPlayer == "Scissors" && randomInt == 1)){
+                    result = "Enemy Wins";
+                }
+                else {
+                    result = "Player Wins";
+                }
+                Console.WriteLine(result);
+            }
+        }        
     }
 }
